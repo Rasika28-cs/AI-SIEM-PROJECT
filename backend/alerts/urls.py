ad0627238886b319d195bb69c7d0fe439816bb85
Aashigasree <aashigasree@gmail.com>
2026-07-29 20:24:42 +0530

    Created alert api

--- backend/alerts/urls.py
+++ backend/alerts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from .detail_api import alert_detail_api, update_status_api
+from .api import alerts_api
 from . import views
 
 urlpatterns = [
     path("", views.alerts_view, name="alerts"),
+    path("api/", alerts_api, name="alerts_api"),
     path("<int:pk>/", views.alert_detail, name="alert_detail"),
     path("<int:pk>/status/", views.update_status, name="update_status"),
+    path("api/<int:pk>/", alert_detail_api, name="alert_detail_api"),
+    path("api/<int:pk>/status/", update_status_api, name="update_status_api"),
 ]
