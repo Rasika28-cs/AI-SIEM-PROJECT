9b782c44e3f1f14d9eb1510b28fd2f32b7ff0f24
Aashigasree <aashigasree@gmail.com>
2026-07-29 15:46:42 +0530

    Create api.py in logs

--- backend/logs/urls.py
+++ backend/logs/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
+from .api import logs_api
 from .views import logs_view
 
 urlpatterns = [
     path("", logs_view, name="logs"),
+    path("api/", logs_api, name="logs_api")
 ]
